9698715ae8cee86f7ee018d7447fc14557317134
Ashish Chauhan <ashish@symbyotic.com-at-349015593822>
2019-03-19 00:06:00 +0530

    Added source files
    Added source files

---
+++ GrowingCellTextView/GrowingCellTextView/GrowingCell.swift
@@ -0,0 +1,42 @@
+//
+//  GrowingCell.swift
+//  GrowingCellTextView
+//
+//  Created by SwiftDevCenter on 12/03/19.
+//  Copyright © 2019 SwiftDevCenter. All rights reserved.
+//
+
+import UIKit
+
+protocol GrowingCellProtocol: class {
+    func updateHeightOfRow(_ cell: GrowingCell, _ textView: UITextView)
+}
+
+class GrowingCell: UITableViewCell {
+    
+    
+    weak var cellDelegate: GrowingCellProtocol?
+    @IBOutlet weak var textView: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        textView.delegate = self
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
+
+extension GrowingCell: UITextViewDelegate {
+    
+    func textViewDidChange(_ textView: UITextView) {
+        if let deletate = cellDelegate {
+            deletate.updateHeightOfRow(self, textView)
+        }
+    }
+}
---
+++ GrowingCellTextView/GrowingCellTextView/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  GrowingCellTextView
+//
+//  Created by SwiftDevCenter on 12/03/19.
+//  Copyright © 2019 SwiftDevCenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let nib = UINib(nibName: "GrowingCell", bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: "GrowingCell")
+        self.tableView.tableFooterView = UIView()
+        self.tableView.dataSource = self
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GrowingCell", for: indexPath) as! GrowingCell
+        cell.cellDelegate = self
+        return cell
+    }
+    
+}
+
+extension ViewController: GrowingCellProtocol {
+    
+    // Update height of UITextView based on string height
+    func updateHeightOfRow(_ cell: GrowingCell, _ textView: UITextView) {
+        let size = textView.bounds.size
+        let newSize = tableView.sizeThatFits(CGSize(width: size.width,
+                                                        height: CGFloat.greatestFiniteMagnitude))
+        if size.height != newSize.height {
+            UIView.setAnimationsEnabled(false)
+            tableView?.beginUpdates()
+            tableView?.endUpdates()
+            UIView.setAnimationsEnabled(true)
+            if let thisIndexPath = tableView.indexPath(for: cell) {
+                tableView.scrollToRow(at: thisIndexPath, at: .bottom, animated: false)
+            }
+        }
+    }
+}
+
